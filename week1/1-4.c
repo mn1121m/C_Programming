68c2a01a3b55d22be13242f4f3c60d566e46e03c
mn1121m <jymoon1121@gmail.com>
2021-05-11 17:56:05 +0900

    updata Week1-1

--- week1/1-4.c
+++ week1/1-4.c
@@ -1,13 +1,18 @@
 /* 1.pdf - 문제 4 */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-#define SQ2(x) ((x)*(x))
+#include "mymacro.h"
 
 int main(void) 
 {
+    printf("결과1:\n");
+    printf("2\n");
+    printf("%d\n", SQ4(2));
+    printf("\n");
 
+    printf("결과2:\n");
+    printf("10\n");
+    printf("%d\n", SQ4(10));
 
     return 0;
 }
---
+++ week1/1-5.c
@@ -0,0 +1,39 @@
+/* 1.pdf - 문제 5 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int money;
+    printf("거스름돈을 입력하세요 >> ");
+    scanf("%d", &money);
+
+    printf("거스름돈 28777은 ");
+    printf("만원 %d장", money / 10000);
+    money = money % 10000;
+    printf("오천원 %d장 ", money / 5000);
+    money = money % 5000;
+    printf("천원 %d장 ", money / 1000);
+    money = money % 1000;
+    printf("오백원 %d개 ", money / 500);
+    money = money % 500;
+    printf("백원 %d개 ", money / 100);
+    money = money % 100;
+    printf("오십원 %d개 ", money / 50);
+    money = money % 50;
+    printf("십원 %d개 ", money / 10);
+    money = money % 10;
+    printf("오원 %d개 ", money / 5);
+    money = money % 5;
+    printf("일원 %d개입니다.\n", money / 1);
+    money = money % 1;
+    
+    /*
+    결괴:
+    거스름돈을 입력하세요 >> 28777
+    거스름돈 28777은 만원 2장오천원 1장 천원 3장 오백원 1개 백원 2개 오십원 1개 십원 2개 오원 1개 일원 2개입니다.
+    */
+
+    return 0;
+}
