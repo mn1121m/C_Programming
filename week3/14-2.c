99c2686946a0ae05047a03d30511209b5176b93b
mn1121m <jymoon1121@gmail.com>
2021-05-27 14:11:56 +0900

    update Week3

--- week3/14-2.c
+++ week3/14-2.c
@@ -3,3 +3,21 @@ Practice14 -2
 2. 정수 형태의 문자열을 정수 값으로 바꾸는 함수를 만드시오. 입력한 수는 라이브러리 함수 atoi() 및 사용자 정의 함수를 사용하여 각각 정수로 변환하여 15를 더하여 출력한다. 
 예를들어, 실행 시 문자열 “1234”를 입력받아 정수 1234 형태로 바꾸고 1234+15를 계산하여 1249를 출력하여야 한다. (2점)
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STR 100
+
+int main(void)
+{
+    char num_str[MAX_STR] = {0, };
+    int num;
+
+    printf("문자열 입력 : ");
+    scanf("%s", num_str);
+    num = atoi(num_str);
+
+    printf("%d + 15 = %d\n", num, num+15);
+
+    return 0;
+}
--- week3/14-3.c
+++ week3/14-3.c
@@ -5,3 +5,66 @@ Practice14 -3
 사용자는 최소 5자리 그리고 최대 10자리의 비밀번호를 입력해야하며 사용자가 입력한 값 대신에 ‘*’가 화면에 찍혀야 하며 
 5자리 미만과 10자리 이상인 경우도 “Retry”를 출력하고 다시 시작한다. (2점)
 */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_PWD_ARY 100
+#define TRUE 1
+
+int main(void)
+{   
+    char pwd_ary[MAX_PWD_ARY] = {0, };
+    char pwd2_ary[MAX_PWD_ARY] = {0, };
+    int i;
+    
+    while(TRUE) {
+        printf("비밀번호 입력 : ");
+        scanf("%s", pwd_ary);
+        
+        for(i = 0; i < strlen(pwd_ary); i++) {
+            printf("*");
+        }
+        printf("\n");
+        if(strlen(pwd_ary) < 5 || strlen(pwd_ary) > 10) {
+            printf("Retry\n"); 
+            continue;
+        }
+
+        printf("비밀번호 다시입력 : ");
+        scanf("%s", pwd2_ary);
+        for(i = 0; i < strlen(pwd2_ary); i++) {
+            printf("*");
+        }
+        printf("\n");
+        if(strlen(pwd2_ary) < 5 || strlen(pwd2_ary) > 10) {
+            printf("Retry\n"); 
+            continue;
+        }
+        //비교
+        if(strcmp(pwd_ary, pwd2_ary)) {
+            printf("Retry\n");
+        }else
+            break;
+    }
+    printf("종료\n");
+    return 0;
+}
+ /*
+    비밀번호 입력 : 123456
+    ******
+    비밀번호 다시입력 : 123457
+    ******
+    Retry
+    비밀번호 입력 : 123
+    ***
+    Retry
+    비밀번호 입력 : 12345678910
+    ***********
+    Retry
+    비밀번호 입력 : 12345
+    *****
+    비밀번호 다시입력 : 12345
+    *****
+    종료
+*/
